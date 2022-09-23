01a1357f7be0c3b7f0d23862c59c8cefc841a3e5
janvandenpoelatAP <jan.vandenpoel@ap.be>
2022-09-23 12:49:22 +0200

    2223 Les 02, 03 Debugging

---
+++ Les 03 Oefeningen Debugging/OefeningDebugging5/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string w;
+int i, t, ttl;
+List<TimeSheetEntry> ents = new List<TimeSheetEntry>();
+Console.Write("Wat heb je gedaan, voor wie?");
+w = Console.ReadLine();
+Console.Write("Hoe lang heb je gewerkt?");
+t = int.Parse(Console.ReadLine());
+TimeSheetEntry ent = new TimeSheetEntry();
+ent.HoursWorked = t;
+ent.WorkDone = w;
+ents.Add(ent);
+Console.Write("Wil je nog een tijdeenheid ingeven?");
+bool cont = bool.Parse(Console.ReadLine());
+do
+{
+    Console.Write("Wat heb je gedaan, voor wie?");
+    w = Console.ReadLine();
+    Console.Write("Hoe lang heb je gewerkt?");
+    t = int.Parse(Console.ReadLine());
+    ent.HoursWorked = t;
+    ent.WorkDone = w;
+    ents.Add(ent);
+    Console.Write("Wil je nog een tijdeenheid ingeven?");
+    cont = bool.Parse(Console.ReadLine());
+} while (cont == true);
+ttl = 0;
+for (i = 0; i < ents.Count; i++)
+{
+    if (ents[i].WorkDone.Contains("AP"))
+    {
+        ttl += i;
+    }
+}
+Console.WriteLine("Simulatie zenden email naar AP");
+Console.WriteLine("De rekening is €" + ttl * 150 + " voor de gewerkte uren.");
+for (i = 0; i < ents.Count; i++)
+{
+    if (ents[i].WorkDone.Contains("Microsoft"))
+    {
+        ttl += i;
+    }
+}
+Console.WriteLine("Simulatie zenden email naar Microsoft");
+Console.WriteLine("De rekening is €" + ttl * 125 + " voor de gewerkte uren.");
+for (i = 0; i < ents.Count; i++)
+{
+    ttl += ents[i].HoursWorked;
+}
+if (ttl > 40)
+{
+    Console.WriteLine("Je verdient €" + ttl * 15 + " voor je werk.");
+}
+else
+{
+    Console.WriteLine("Je verdient €" + ttl * 10 + " voor je werk.");
+}
+Console.WriteLine();
+Console.Write("Druk op een toets om de applicatie af te sluiten...");
+Console.ReadKey();
+
+
+public class TimeSheetEntry
+{
+    public string WorkDone;
+    public int HoursWorked;
+}
+
--- Les 03 Oefeningen Debugging/VoorbeeldDebugging2/Program.cs
+++ Les 03 Oefeningen Debugging/VoorbeeldDebugging2/Program.cs
@@ -1,63 +1,58 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
 using System.Collections.Generic;
+using System;
 
-namespace VoorbeeldDebugging2
+Console.WriteLine("Welcome to Galaxy News!");
+
+IterateThroughList();
+Console.ReadKey();
+
+static void IterateThroughList()
 {
-    class Program
+    var theGalaxies = new List<Galaxy>
+     {
+     new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+     new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+     new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+     new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+     new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+     new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+     };
+    foreach (Galaxy theGalaxy in theGalaxies)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Galaxy News!");
-            IterateThroughList();
-            Console.ReadKey();
-        }
-
-        private static void IterateThroughList()
-        {
-            var theGalaxies = new List<Galaxy>
-            {
-                new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
-                new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
-                new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
-                new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
-                new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
-                new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
-            };
-            foreach (Galaxy theGalaxy in theGalaxies)
-            {
-                Console.WriteLine(theGalaxy.Name + " " + theGalaxy.MegaLightYears + ", " + theGalaxy.GalaxyType);
-            }
-        }
-    }
-    public class Galaxy
-    {
-        public string Name { get; set; }
-        public double MegaLightYears { get; set; }
-        public object GalaxyType { get; set; }
-    }
-    public class GType
-    {
-        public GType(char type)
-        {
-            switch (type)
-            {
-                case 'S':
-                    MyGType = Type.Spiral;
-                    break;
-                case 'E':
-                    MyGType = Type.Elliptical;
-                    break;
-                case 'l':
-                    MyGType = Type.Irregular;
-                    break;
-                case 'L':
-                    MyGType = Type.Lenticular;
-                    break;
-                default:
-                    break;
-            }
-        }
-        public object MyGType { get; set; }
-        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
+        Console.WriteLine(theGalaxy.Name + " " + theGalaxy.MegaLightYears + ", " + theGalaxy.GalaxyType);
     }
 }
+
+public class Galaxy
+{
+    public string Name { get; set; }
+    public double MegaLightYears { get; set; }
+    public object GalaxyType { get; set; }
+}
+public class GType
+{
+    public GType(char type)
+    {
+        switch (type)
+        {
+            case 'S':
+                MyGType = Type.Spiral;
+                break;
+            case 'E':
+                MyGType = Type.Elliptical;
+                break;
+            case 'l':
+                MyGType = Type.Irregular;
+                break;
+            case 'L':
+                MyGType = Type.Lenticular;
+                break;
+            default:
+                break;
+        }
+    }
+    public object MyGType { get; set; }
+    private enum Type { Spiral, Elliptical, Irregular, Lenticular }
+}
+
